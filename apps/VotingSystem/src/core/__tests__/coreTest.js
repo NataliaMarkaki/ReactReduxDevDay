32d286c7c1d113e953a6c29317ed4ff32550e901
nataliam <natalia.markaki@caplin.com>
2017-10-12 15:13:20 +0100

    Introduce the voting structure

--- apps/VotingSystem/src/core/__tests__/coreTest.js
+++ apps/VotingSystem/src/core/__tests__/coreTest.js
@@ -1,11 +1,12 @@
 import { List, Map } from "immutable";
 import * as matchers from "jest-immutable-matchers";
-import { setEntries } from "../core";
+import { setEntries, next, vote } from "../core";
 
 describe("application logic", () => {
   beforeEach(function() {
     jest.addMatchers(matchers);
   });
+
   describe("setEntries", () => {
     it("adds the entries to the state", () => {
       const state = Map();
@@ -25,6 +26,139 @@ describe("application logic", () => {
       expect(nextState).toEqualImmutable(
         Map({
           entries: List.of("Trainspotting", "28 Days Later")
+        })
+      );
+    });
+  });
+
+  describe("next", () => {
+    it("takes the next two entries under vote", () => {
+      const state = Map({
+        entries: List.of("Trainspotting", "28 Days Later", "Sunshine")
+      });
+      const nextState = next(state);
+
+      expect(nextState).toEqualImmutable(
+        Map({
+          vote: Map({
+            pair: List.of("Trainspotting", "28 Days Later")
+          }),
+          entries: List.of("Sunshine")
+        })
+      );
+    });
+
+    it("puts winner of current vote back to entries", () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of("Trainspotting", "28 Days Later"),
+          tally: Map({
+            Trainspotting: 4,
+            "28 Days Later": 2
+          })
+        }),
+        entries: List.of("Sunshine", "Millions", "127 Hours")
+      });
+      const nextState = next(state);
+
+      expect(nextState).toEqualImmutable(
+        Map({
+          vote: Map({
+            pair: List.of("Sunshine", "Millions")
+          }),
+          entries: List.of("127 Hours", "Trainspotting")
+        })
+      );
+    });
+
+    it("puts both from tied vote back to entries", () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of("Trainspotting", "28 Days Later"),
+          tally: Map({
+            Trainspotting: 3,
+            "28 Days Later": 3
+          })
+        }),
+        entries: List.of("Sunshine", "Millions", "127 Hours")
+      });
+
+      const nextState = next(state);
+      expect(nextState).toEqualImmutable(
+        Map({
+          vote: Map({
+            pair: List.of("Sunshine", "Millions")
+          }),
+          entries: List.of("127 Hours", "Trainspotting", "28 Days Later")
+        })
+      );
+    });
+
+    it("marks winner when just one entry left", () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of("Trainspotting", "28 Days Later"),
+          tally: Map({
+            Trainspotting: 4,
+            "28 Days Later": 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = next(state);
+
+      expect(nextState).toEqualImmutable(
+        Map({
+          winner: "Trainspotting"
+        })
+      );
+    });
+  });
+
+  describe("vote", () => {
+    it("creates a tally for the voted entry", () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of("Trainspotting", "28 Days Later")
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, "Trainspotting");
+      expect(nextState).toEqualImmutable(
+        Map({
+          vote: Map({
+            pair: List.of("Trainspotting", "28 Days Later"),
+            tally: Map({
+              Trainspotting: 1
+            })
+          }),
+          entries: List()
+        })
+      );
+    });
+
+    it("adds to existing tally for the voted entry", () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of("Trainspotting", "28 Days Later"),
+          tally: Map({
+            Trainspotting: 3,
+            "28 Days Later": 2
+          })
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, "Trainspotting");
+      expect(nextState).toEqualImmutable(
+        Map({
+          vote: Map({
+            pair: List.of("Trainspotting", "28 Days Later"),
+            tally: Map({
+              Trainspotting: 4,
+              "28 Days Later": 2
+            })
+          }),
+          entries: List()
         })
       );
     });
