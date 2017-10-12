7ecff7202d512be47dd726b3d16045506f9d9af3
nataliam <natalia.markaki@caplin.com>
2017-10-12 17:20:30 +0100

    Add immutable data pure rendering

--- apps/VotingClient/src/Voting/Voting.jsx
+++ apps/VotingClient/src/Voting/Voting.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import Winner from '../Winner/Winner';
-import Vote from '../Vote/Vote';
+import Winner from "../Winner/Winner";
+import Vote from "../Vote/Vote";
+import PureRenderMixin from "react-addons-pure-render-mixin";
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
   getPair: function() {
     return this.props.pair || [];
   },
@@ -10,10 +12,14 @@ export default React.createClass({
     return !!this.props.hasVoted;
   },
   render: function() {
-    return <div>
-      {this.props.winner ?
-        <Winner ref="winner" winner={this.props.winner} /> :
-        <Vote {...this.props} />}
-    </div>;
+    return (
+      <div>
+        {this.props.winner ? (
+          <Winner ref="winner" winner={this.props.winner} />
+        ) : (
+          <Vote {...this.props} />
+        )}
+      </div>
+    );
   }
 });
--- apps/VotingClient/src/Voting/__tests__/VotingTest.js
+++ apps/VotingClient/src/Voting/__tests__/VotingTest.js
@@ -6,6 +6,7 @@ import {
   Simulate
 } from "react-addons-test-utils";
 import Voting from "../Voting";
+import { List } from "immutable";
 
 describe("Voting", () => {
   it("renders a pair of buttons", () => {
@@ -19,51 +20,80 @@ describe("Voting", () => {
     expect(buttons[1].textContent).toBe("28 Days Later");
   });
 
-  it('invokes callback when a button is clicked', () => {
+  it("invokes callback when a button is clicked", () => {
     let votedWith;
-    const vote = (entry) => votedWith = entry;
+    const vote = entry => (votedWith = entry);
 
     const component = renderIntoDocument(
-      <Voting pair={["Trainspotting", "28 Days Later"]}
-              vote={vote}/>
+      <Voting pair={["Trainspotting", "28 Days Later"]} vote={vote} />
     );
-    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    const buttons = scryRenderedDOMComponentsWithTag(component, "button");
     Simulate.click(buttons[0]);
 
-    expect(votedWith).toBe('Trainspotting');
+    expect(votedWith).toBe("Trainspotting");
   });
 
-  it('disables buttons when user has voted', () => {
+  it("disables buttons when user has voted", () => {
     const component = renderIntoDocument(
-      <Voting pair={["Trainspotting", "28 Days Later"]}
-              hasVoted="Trainspotting" />
+      <Voting
+        pair={["Trainspotting", "28 Days Later"]}
+        hasVoted="Trainspotting"
+      />
     );
-    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
-  
+    const buttons = scryRenderedDOMComponentsWithTag(component, "button");
+
     expect(buttons.length).toBe(2);
-    expect(buttons[0].hasAttribute('disabled')).toBe(true);
-    expect(buttons[1].hasAttribute('disabled')).toBe(true);
+    expect(buttons[0].hasAttribute("disabled")).toBe(true);
+    expect(buttons[1].hasAttribute("disabled")).toBe(true);
   });
 
-  it('adds label to the voted entry', () => {
+  it("adds label to the voted entry", () => {
     const component = renderIntoDocument(
-      <Voting pair={["Trainspotting", "28 Days Later"]}
-              hasVoted="Trainspotting" />
+      <Voting
+        pair={["Trainspotting", "28 Days Later"]}
+        hasVoted="Trainspotting"
+      />
     );
-    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
-  
-    expect(buttons[0].textContent).toContain('Voted');
+    const buttons = scryRenderedDOMComponentsWithTag(component, "button");
+
+    expect(buttons[0].textContent).toContain("Voted");
   });
 
-  it('renders just the winner when there is one', () => {
-    const component = renderIntoDocument(
-      <Voting winner="Trainspotting" />
-    );
-    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+  it("renders just the winner when there is one", () => {
+    const component = renderIntoDocument(<Voting winner="Trainspotting" />);
+    const buttons = scryRenderedDOMComponentsWithTag(component, "button");
     expect(buttons.length).toBe(0);
-  
+
     const winner = ReactDOM.findDOMNode(component.refs.winner);
     expect(winner).toBeTruthy;
-    expect(winner.textContent).toContain('Trainspotting');
+    expect(winner.textContent).toContain("Trainspotting");
+  });
+
+  it("renders as a pure component", () => {
+    const pair = ["Trainspotting", "28 Days Later"];
+    const container = document.createElement("div");
+    let component = ReactDOM.render(<Voting pair={pair} />, container);
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, "button")[0];
+    expect(firstButton.textContent).toBe("Trainspotting");
+
+    pair[0] = "Sunshine";
+    component = ReactDOM.render(<Voting pair={pair} />, container);
+    firstButton = scryRenderedDOMComponentsWithTag(component, "button")[0];
+    expect(firstButton.textContent).toBe("Trainspotting");
+  });
+
+  it("does update DOM when prop changes", () => {
+    const pair = List.of("Trainspotting", "28 Days Later");
+    const container = document.createElement("div");
+    let component = ReactDOM.render(<Voting pair={pair} />, container);
+
+    let firstButton = scryRenderedDOMComponentsWithTag(component, "button")[0];
+    expect(firstButton.textContent).toBe("Trainspotting");
+
+    const newPair = pair.set(0, "Sunshine");
+    component = ReactDOM.render(<Voting pair={newPair} />, container);
+    firstButton = scryRenderedDOMComponentsWithTag(component, "button")[0];
+    expect(firstButton.textContent).toBe("Sunshine");
   });
 });
