3f3ddfa50db10169df22393899b1de75ac4bb7ec
nataliam <natalia.markaki@caplin.com>
2017-10-12 17:40:53 +0100

    Set up Redux store

--- apps/VotingClient/src/index.js
+++ apps/VotingClient/src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Voting from "./Voting/Voting";
-import Results from './Results/Results';
+import Results from "./Results/Results";
 import { Route } from "react-router";
+import { createStore } from "redux";
+import reducer from "../Reducer/Reducer";
 
-const pair = ["Trainspotting", "28 Days Later"];
+const store = createStore(reducer);
+store.dispatch({
+  type: "SET_STATE",
+  state: {
+    vote: {
+      pair: ["Sunshine", "28 Days Later"],
+      tally: { Sunshine: 2 }
+    }
+  }
+});
 
 const routes = (
   <Route component={App}>
@@ -15,5 +26,5 @@ const routes = (
 
 ReactDOM.render(
   <Router history={hashHistory}>{routes}</Router>,
-  document.getElementById('app')
+  document.getElementById("app")
 );
